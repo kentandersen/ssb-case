aa1f3dba51a74da7ed8633f95e75f3b40696922b
Kent Andersen <kentareandersen@gmail.com>
2017-08-28 22:50:16 +0200

    rendret den enkleste tabellen mulig

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
-console.log('SSB case');
+import { getFreshExports } from './service';
+
+function renderRow({week, pricePrKg, weight}) {
+  return `
+    <tr>
+      <td>${week}</td>
+      <td>${weight}</td>
+      <td>${pricePrKg}</td>
+    </tr>
+  `;
+}
+
+
+function render(data) {
+  const html = `
+    <table>
+      <thead>
+        <tr>
+          <th>Uke</th>
+          <th>Vekt (tonn)</th>
+          <th>Kilopris (kr)</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${data.map(renderRow).join('')}
+      </tbody>
+    </table>
+  `;
+
+  document.querySelector('main').innerHTML = html;
+}
+
+
+getFreshExports().then(render);
---
+++ src/service.js
@@ -0,0 +1,6 @@
+export function getFreshExports() {
+  return Promise.resolve([
+    { week: "2017U32", pricePrKg: 17373, weight: 59.53 },
+    { week: "2017U33", pricePrKg: 17023, weight: 55.63 }
+  ]);
+}
