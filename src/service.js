cfebb5c957cf26729aeef0c251b01ef5fc7061c1
Kent Andersen <kentareandersen@gmail.com>
2017-08-29 21:19:07 +0200

    koble til ssb api

--- src/service.js
+++ src/service.js
@@ -1,6 +1,31 @@
+import JSONstat from 'jsonstat';
+
+// Eksport av oppalen laks, etter varegruppe, tid og statistikkvariabel siste to uker
+const url = 'http://data.ssb.no/api/v0/dataset/1120.json?lang=no';
+
+// Eksport av oppalen laks, etter varegruppe, tid og statistikkvariabel fra 2000
+// const url = 'http://data.ssb.no/api/v0/dataset/1122.json?lang=no'
+
+
+function parse(data) {
+  const stat = JSONstat(data);
+  const dataset = stat.Dataset(0);
+
+  const returnArray = [];
+  for (var i = 0; dataset.Dimension(1).Category(i); i++) {
+    returnArray.push({
+      week: dataset.Dimension(1).Category(i).label,
+      weightInKg: dataset.Data([0, i, 0]).value,
+      pricePrKg: dataset.Data([0, i, 1]).value
+    });
+  }
+
+  return returnArray;
+}
+
 export function getFreshExports() {
-  return Promise.resolve([
-    { week: "2017U32", weightInKg: 17373, pricePrKg: 59.53 },
-    { week: "2017U33", weightInKg: 17023, pricePrKg: 55.63 }
-  ]);
+
+  return fetch(url)
+    .then(res => res.json())
+    .then(parse);
 }
